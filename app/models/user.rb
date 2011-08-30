c9c062c6ed9cf970724ecb0b17c0c0b29e9dee88
Brian Collins <brian@gastownlabs.com>
2011-08-30 17:57:02 +0100

    Last 4 digits saved in database can no longer be faked by the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   attr_accessor :password, :stripe_token
   before_save :encrypt_password
-  before_save :create_stripe_customer
+  before_save :update_stripe
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
@@ -13,22 +13,26 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_presence_of :last_4_digits
 
-  def create_stripe_customer
+  def update_stripe
     if stripe_token.present?
       if stripe_id.nil?
         customer = Stripe::Customer.create(
           :description => email,
           :card => stripe_token
         )
+        self.last_4_digits = customer.active_card.last4
         response = customer.update_subscription({:plan => "premium"})
       else
         customer = Stripe::Customer.retrieve(stripe_id)
         customer.card = stripe_token
         customer.save
+        self.last_4_digits = customer.active_card.last4
       end
 
       self.stripe_id = customer.id
       self.stripe_token = nil
+    elsif last_4_digits_changed?
+      self.last_4_digits = last_4_digits_was
     end
   end
 
