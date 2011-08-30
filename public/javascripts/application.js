0b4003ae081f6b4a82b97d564debec48db138ef5
Brian Collins <brian@gastownlabs.com>
2011-08-30 17:57:22 +0100

    Fix selector for update credit card form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 $(function() {
   $("#credit-card input, #credit-card select").attr("disabled", false);
 
-  $("#new_user, .edit_user").submit(function() {
+  $("form:has(#credit-card)").submit(function() {
     var form = this;
     $("#user_submit").attr("disabled", true);
     $("#credit-card input, #credit-card select").attr("name", "");
